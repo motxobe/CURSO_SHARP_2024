269024cf12070c8775fa567b95e485b8876d0ce3
motxobe <72982830+motxobe@users.noreply.github.com>
2024-04-29 14:35:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("hola mundo");
+            Console.Write("Introduzca un texto ");
+            String texto;
+            texto = Console.ReadLine();
+            Console.WriteLine("El texto introducido es: " + texto);
+            Console.WriteLine("PULSA UNA TECLA PARA CONTINUAR");
+            texto = Console.ReadLine();
+
+            DateTime fecha;
+            DateTime currentDate = DateTime.Now;
+            fecha = DateTime.Now;
+            int dia, mes, anio;
+            dia = fecha.Day;
+            mes = fecha.Month;
+            anio = fecha.Year;
+
+            Console.Write(Environment.NewLine + "Today is " + dia + "/" + mes + "/" + anio);
+            Console.Write(Environment.NewLine + "Press a key to exit... ");
+            Console.ReadLine(); // Console.ReadKey(true);
+
+
+        }
+    }
+}
